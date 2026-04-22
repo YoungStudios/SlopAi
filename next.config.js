cf4922a9836e5352dd6c790e1c5fad71619c5c8b
YoungStudios <northchicagorp0@gmail.com>
2026-04-22 08:18:27 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/index.html'
+      }
+    ]
+  }
+}
 module.exports = nextConfig
